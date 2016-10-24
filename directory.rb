cf1892733a2447ac173520b7b92b997aa246bd26
Tom Fuller <tomf95@hotmail.co.uk>
2016-10-24 10:59:13 +0100

    refactored list

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villians Academy"
 puts "-------------"
 puts "Dr Hannibal Lecter"
@@ -12,6 +14,4 @@ puts "The Joker"
 puts "Jeoffrey Baratheon"
 puts "Norman Bates"
 
-print "Overall we have "
-print 11
-puts " great students."
+puts "Overall we have #{student_count} great students."
