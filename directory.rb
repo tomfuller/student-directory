31a4f16b3b3ee639686d07c22605037b03b9a40e
Tom Fuller <tomf95@hotmail.co.uk>
2016-10-25 12:45:16 +0100

    Interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,32 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu and ask user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Read the input and save it to a variable
+    selection = gets.chomp
+    # 3. What the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_by_cohorts(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant. Try again."
+    end
+
+  end
+end
+
+
+
+
 
 def print_header
   puts "The students of Villians Academy".center(50)
@@ -62,7 +91,7 @@ def print_by_cohorts(students)
   arr.sort.uniq.each { |cohort| print_names(students.select { |student| student.has_value?(cohort)})}
 end
 
-students = input_students
-print_header
-print_by_cohorts(students)
-print_footer(students)
+interactive_menu
+#print_header
+#print_by_cohorts(students)
+#print_footer(students)
