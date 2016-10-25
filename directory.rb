7d7a2ec023ca1ba3d2a1b59dd11b9dd6dcb87c6a
Tom Fuller <tomf95@hotmail.co.uk>
2016-10-25 13:44:52 +0100

    refactoring done

--- directory.rb
+++ directory.rb
@@ -1,32 +1,36 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # 1. Print the menu and ask user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. Read the input and save it to a variable
-    selection = gets.chomp
-    # 3. What the user has asked
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_by_cohorts
+  print_footer
+end
+
+def process(selection)
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print_by_cohorts(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant. Try again."
     end
-
-  end
 end
-
-
-
-
 
 def print_header
   puts "The students of Villians Academy".center(50)
@@ -46,16 +50,14 @@ def print_names(names)
   end
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.count} great students.".center(50)
+def print_footer
+  puts "Overall we have #{@students.count} great students.".center(50)
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice."
-
-  students = []
-
+  #@students = []
   name = gets.gsub(/\n/, "").to_sym
   puts "Please enter their Cohort (Note: Must be Sep, Nov or Jan otherwise will be put in unknow catagory.)"
   cohort = gets.chomp.to_sym
@@ -63,15 +65,13 @@ def input_students
   else
     cohort = :unknown
   end
-
-
   while !name.empty? do
-    students << {name: name, cohort: cohort, hobby: :coding, height: :'175', country_of_birth: :Spain}
+    @students << {name: name, cohort: cohort, hobby: :coding, height: :'175', country_of_birth: :Spain}
 
-    if students.count > 1
-      puts "Now we have #{students.count} students"
+    if @students.count > 1
+      puts "Now we have #{@students.count} students"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
     puts "Same again. Name:"
     name = gets.chomp.to_sym
@@ -82,16 +82,13 @@ def input_students
       cohort = :unknown
     end
   end
-  students
 end
 
-def print_by_cohorts(students)
+
+def print_by_cohorts
   arr = []
-  arr = students.map { |e| e[:cohort] }
-  arr.sort.uniq.each { |cohort| print_names(students.select { |student| student.has_value?(cohort)})}
+  arr = @students.map { |e| e[:cohort] }
+  arr.sort.uniq.each { |cohort| print_names(@students.select { |student| student.has_value?(cohort)})}
 end
 
 interactive_menu
-#print_header
-#print_by_cohorts(students)
-#print_footer(students)
